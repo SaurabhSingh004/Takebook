afaa02e10831e52e3b194d347e8b5cfe817611d9
VaderX <shuklapriyanshu9@gmail.com>
2020-08-21 11:58:49 +0530

    Initial Commit

--- chat/settings.py
+++ chat/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'tlq#n6ny5tvtkall!_ozh46p&l3i16k8o@3eime@x+0o@(b8$p'
+SECRET_KEY = 'your Security Key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
